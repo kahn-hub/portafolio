db45be5d7ce8d0cb8f0538b0edb917992eef1d4c
dumas parraga <dumas_parraga1999@gmail.com>
2021-10-03 15:08:31 -0500

    Ya le agregue algo de javascript

---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector("#menu")
+const menu = document.querySelector("#menu-despleg")
+menuIcon.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+});
